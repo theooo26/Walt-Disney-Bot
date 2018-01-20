93e3c905c2fe0462d0e2fc40e054058b9cf479da
theooo26 <35627078+theooo26@users.noreply.github.com>
2018-01-20 14:10:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.channel.send('PONG!');
+    	message.channel.send('**PONG!**');
   	}
 });
 
